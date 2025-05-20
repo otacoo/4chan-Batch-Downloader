7b9e5671a313f8d6a21f6ef94376261074ab7417
otacoo <208893667+otacoo@users.noreply.github.com>
2025-05-20 04:03:24 +0100

    Create zip file for Firefox instead of XPI

--- gruntfile.js
+++ gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+module.exports = function (grunt) {
   const esbuild = require('esbuild');
 
   grunt.initConfig({
@@ -6,37 +6,51 @@ module.exports = function(grunt) {
       chrome: {
         files: [
           // Copy all source files except manifests and background.js
-          {expand: true, cwd: 'src/', src: ['**', '!manifest.*.json', '!background.js'], dest: 'dist/chrome/'},
+          { expand: true, cwd: 'src/', src: ['**', '!manifest.*.json', '!background.js'], dest: 'dist/chrome/' },
           // Copy Chrome manifest
-          {src: 'src/manifest.chrome.json', dest: 'dist/chrome/manifest.json'},
+          { src: 'src/manifest.chrome.json', dest: 'dist/chrome/manifest.json' },
           // Copy icons
-          {expand: true, cwd: 'icons/', src: ['**'], dest: 'dist/chrome/icons/'},
+          { expand: true, cwd: 'icons/', src: ['**'], dest: 'dist/chrome/icons/' },
           // Copy background.js as-is for Chrome (ESM)
-          {src: 'src/background.js', dest: 'dist/chrome/background.js'}
+          { src: 'src/background.js', dest: 'dist/chrome/background.js' }
         ]
       },
       firefox: {
         files: [
           // Copy all source files except manifests and background.js
-          {expand: true, cwd: 'src/', src: ['**', '!manifest.*.json', '!background.js'], dest: 'dist/firefox/'},
+          { expand: true, cwd: 'src/', src: ['**', '!manifest.*.json', '!background.js'], dest: 'dist/firefox/' },
           // Copy Firefox manifest
-          {src: 'src/manifest.firefox.json', dest: 'dist/firefox/manifest.json'},
+          { src: 'src/manifest.firefox.json', dest: 'dist/firefox/manifest.json' },
           // Copy icons
-          {expand: true, cwd: 'icons/', src: ['**'], dest: 'dist/firefox/icons/'}
+          { expand: true, cwd: 'icons/', src: ['**'], dest: 'dist/firefox/icons/' }
           // background.js will be bundled by esbuild
         ]
       }
     },
     clean: {
       build: ['dist/chrome', 'dist/firefox']
+    },
+    zip: {
+      chrome: {
+        cwd: 'dist/chrome/',
+        src: ['dist/chrome/**'],
+        dest: 'dist/4chan-multi-image-downloader-chrome.zip'
+      },
+      firefox: {
+        cwd: 'dist/firefox/',
+        src: ['dist/firefox/**'],
+        dest: 'dist/4chan-multi-image-downloader-firefox.zip', // <-- changed from .xpi to .zip
+        compression: 'DEFLATE'
+      }
     }
   });
 
+  grunt.loadNpmTasks('grunt-zip');
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
 
   // Esbuild task for Firefox background.js
-  grunt.registerTask('esbuild:firefox', 'Bundle background.js for Firefox', function() {
+  grunt.registerTask('esbuild:firefox', 'Bundle background.js for Firefox', function () {
     const done = this.async();
     esbuild.build({
       entryPoints: ['src/background.js'],
@@ -53,4 +67,7 @@ module.exports = function(grunt) {
   grunt.registerTask('build:chrome', ['clean', 'copy:chrome']);
   grunt.registerTask('build:firefox', ['clean', 'copy:firefox', 'esbuild:firefox']);
   grunt.registerTask('build', ['clean', 'copy:chrome', 'copy:firefox', 'esbuild:firefox']);
+  grunt.registerTask('pack:chrome', ['build:chrome', 'zip:chrome']);
+  grunt.registerTask('pack:firefox', ['build:firefox', 'zip:firefox']);
+  grunt.registerTask('pack', ['build', 'zip:chrome', 'zip:firefox']); // Builds and zips both
 };
